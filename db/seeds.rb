2731fd1727c11ab7dba6337190fb4d23fcd3f095
Adele Soullier <adele.soullier@gmail.com>
2019-05-15 18:43:58 +0200

    created model and empty controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Flat.destroy_all
+
+8.times do
+  Flat.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: rand(23..320),
+    number_of_guests: rand(1..12)
+  )
+end
